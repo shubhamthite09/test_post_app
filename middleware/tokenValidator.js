f20197382bd81e6b8cd23e5317bbfcf0fae546e2
Thite Shubham <115460300+shubhamthite09@users.noreply.github.com>
2023-04-19 20:19:23 +0530

    Update tokenValidator.js

--- middleware/tokenValidator.js
+++ middleware/tokenValidator.js
@@ -5,46 +5,6 @@ const {blackModle} = require("../modles/blacklist");
 
 const validator = (req, res, next) => {
   console.log(req.cookies)
-  //const token = req.cookies.token || req.headers.authorization.token
-  //const refresh_token = req.cookies.refresh_token || req.headers.authorization.refresh_token
-  //console.log(token,refresh_token)
-  //jwt.verify(token, process.env.token_key, (err, decoded) => {
-    if (err) {
-      if (refresh_token) {
-        jwt.verify(refresh_token, process.env.refresh_key, async (err, decoded) => {
-          if (err) {
-            res
-              .status(401)
-              .json({ error: `please login again your token is not valid` });
-          } else {
-            if (await blackModle.findOne({ token:refresh_token })) {
-              res
-                .status(403)
-                .json({ error: `please login you are in blacklist` });
-            } else {
-              let token = jwt.sign(
-                { id: decoded.id, role: decoded.role },
-                process.env.token_key,
-                { expiresIn: "1h" }
-              );
-              res.cookie("token", token);
-              req.body.user = decoded.id;
-              req.body.role = decoded.role;
-              next();
-            }
-          }
-        });
-      } else {
-        res
-          .status(406)
-          .json({ error: `please login again your token is not valid` });
-      }
-    } else {
-      req.body.user = decoded.id;
-      req.body.role = decoded.role;
-      next();
-    }
- // });
 };
 
 const authorization = (req, res, next) => {
